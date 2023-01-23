6996f6c312cff5da9fa020e93030528e04f0b30d
algnot <thanawat.k2000@gmail.com>
2023-01-24 02:04:07 +0700

    fixed style

--- pages/[id].js
+++ pages/[id].js
@@ -1,41 +1,40 @@
 import { useEffect, useState } from "react";
 import style from "../styles/index.module.css";
 import { useRouter } from "next/router";
-import { Head } from "next/head";
+import Head from "next/head";
 
 export default function Index() {
   const router = useRouter();
   const { id } = router.query;
   const [amount, setAmount] = useState(0);
-  const [href, setHref] = useState("")
+  const [href, setHref] = useState("");
 
   useEffect(() => {
-    setHref(window.location)
-  }, [])
+    setHref(window.location);
+  }, []);
 
   return (
     <>
-    <Head>
-      <title>Promptpay {id}</title>
-    </Head>
-    <div className="container">
-      <div className={style.container}>
-        <h1>Promptpay generator</h1>
-        <input
-          value={amount}
-          type="number"
-          min={0}
-          placeholder="Your amount"
-          onChange={(e) => setAmount(e.target.value)}
-        />
-        <div className={style.card}>
-          <img src={`/api/?id=${id}&amount=${amount}`} />
+      <Head>
+        <title>Promptpay {id}</title>
+      </Head>
+      <div className="container">
+        <div className={style.container}>
+          <h1>Promptpay generator</h1>
+          <input
+            value={amount}
+            type="number"
+            min={0}
+            placeholder="Your amount"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <div className={style.card}>
+            <img src={`/api/?id=${id}&amount=${amount}`} />
+          </div>
+          <div className={style.bold}>Promptpay id : {id}</div>
+          <input className={style.copy} value={`${href}/${amount}`} disabled />
         </div>
-        <div className={style.bold}>Promptpay id : {id}</div>
-        <input className={style.copy} value={`${href}/${amount}`} 
-               disabled/>
       </div>
-    </div>
     </>
   );
 }
--- pages/[id]/[amount].js
+++ pages/[id]/[amount].js
@@ -1,31 +1,35 @@
-import style from '../../styles/index.module.css'
-import { useRouter } from 'next/router'
-import { Head } from "next/head";
+import style from "../../styles/index.module.css";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function Index() {
-  const router = useRouter()
-  const { id, amount } = router.query
+  const router = useRouter();
+  const { id, amount } = router.query;
 
   return (
     <>
-    <Head>
-      <title>Promptpay {id} | {amount}</title>
-    </Head>
-    <div className="container">
-      <div className={style.container}>
-        <h1>Promptpay generator</h1>
-        <input value={amount}
-               type="number"
-               min={0}
-               placeholder="Your amount"
-               onChange={(e) => setAmount(e.target.value)}
-               disabled/>
-        <div className={style.card}>
-          <img src={`/api/?id=${id}&amount=${amount}`}/>
+      <Head>
+        <title>
+          Promptpay {id} | {amount}
+        </title>
+      </Head>
+      <div className="container">
+        <div className={style.container}>
+          <h1>Promptpay generator</h1>
+          <input
+            value={amount}
+            type="number"
+            min={0}
+            placeholder="Your amount"
+            onChange={(e) => setAmount(e.target.value)}
+            disabled
+          />
+          <div className={style.card}>
+            <img src={`/api/?id=${id}&amount=${amount}`} />
+          </div>
+          <div className={style.bold}>Promptpay id : {id}</div>
         </div>
-        <div className={style.bold}>Promptpay id : {id}</div>
       </div>
-    </div>
     </>
-  )
+  );
 }
